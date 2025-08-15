54a007f1216d7d573d649467dd9973470f6d48b8
maroo <marwanapk28@gmail.com>
2025-08-15 04:55:56 +0300

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 
-// Protect routes
+
 exports.protect = async (req, res, next) => {
   let token;
 
@@ -9,15 +9,12 @@ exports.protect = async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
-    // Set token from Bearer token in header
+    
     token = req.headers.authorization.split(' ')[1];
   }
-  // Set token from cookie (optional)
-  // else if (req.cookies.token) {
-  //   token = req.cookies.token;
-  // }
+ }
 
-  // Make sure token exists
+
   if (!token) {
     return res.status(401).json({ success: false, error: 'Not authorized to access this route' });
   }
