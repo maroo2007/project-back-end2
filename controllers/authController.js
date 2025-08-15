0ac31e89ec62902fb897619cf54cccbf333035bf
maroo <marwanapk28@gmail.com>
2025-08-15 04:53:58 +0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,19 @@
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
 
-// Helper function to sign JWT
+
 const getSignedJwtToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: '30d',
   });
 };
 
-// @desc    Register a new user (signup)
-// @route   POST /api/auth/signup
-// @access  Public
+
 exports.signup = async (req, res) => {
   try {
     const { name, email, password, role } = req.body;
 
-    // Create user
+    
     const user = await User.create({
       name,
       email,
@@ -32,9 +30,6 @@ exports.signup = async (req, res) => {
   }
 };
 
-// @desc    Login user
-// @route   POST /api/auth/login
-// @access  Public
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -67,12 +62,9 @@ exports.login = async (req, res) => {
   }
 };
 
-// @desc    Get current logged in user
-// @route   GET /api/auth/me
-// @access  Private
+
 exports.getMe = async (req, res) => {
-  // This function will be used for a protected route.
-  // The user object will be attached to req by the auth middleware.
+  
   const user = await User.findById(req.user.id);
 
   res.status(200).json({
